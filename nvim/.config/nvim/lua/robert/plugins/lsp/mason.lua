1c931285920da2434d0280f25a562c7657fd56c7
Robert <rathmannroberto@gmail.com>
2025-03-20 21:18:32 +0100

    update

--- nvim/.config/nvim/lua/robert/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/robert/plugins/lsp/mason.lua
@@ -1,60 +1,60 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        -- import mason
+        local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+        local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+        -- enable mason and configure icons
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        "rust_analyzer",
-        "clangd",
-        "cmake",
-        "rust_analyzer",
-      },
-    })
+        mason_lspconfig.setup({
+            -- list of servers for mason to install
+            ensure_installed = {
+                "html",
+                "cssls",
+                "tailwindcss",
+                "svelte",
+                "lua_ls",
+                "graphql",
+                "emmet_ls",
+                "prismals",
+                "pyright",
+                "rust_analyzer",
+                "clangd",
+                "cmake",
+                "rust_analyzer",
+                "zls",
+            },
+        })
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-        "debugpy",
-        "clang-format", --cpp formatter
-        "codelldb",
-      },
-    })
-  end,
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "eslint_d",
+                "debugpy",
+                "clang-format", --cpp formatter
+                "codelldb",
+            },
+        })
+    end,
 }
